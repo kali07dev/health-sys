2dfffff74e813d1761a832f5133d181dda5c6472
Hope <hopekalitera@gmail.com>
2025-02-04 02:13:55 +0200

    logger

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -2,12 +2,15 @@ package utils
 
 import (
 	"os"
+
+	"github.com/getsentry/sentry-go"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var Logger *logrus.Logger
 
-func InitLogger(logLevel string, logFormat string) {
+func InitLogger(logLevel string, logFormat string, logFile string, sentryDSN string) {
 	Logger = logrus.New()
 
 	// Set log level
@@ -38,36 +41,59 @@ func InitLogger(logLevel string, logFormat string) {
 		})
 	}
 
-	// Set output to stdout
-	Logger.SetOutput(os.Stdout)
+	// Set log output
+	if logFile != "" {
+		// Use lumberjack for log rotation
+		Logger.SetOutput(&lumberjack.Logger{
+			Filename:   logFile, // Log file path
+			MaxSize:    100,     // Max size in MB before rotation
+			MaxBackups: 3,       // Max number of old log files to retain
+			MaxAge:     28,      // Max number of days to retain log files
+			Compress:   true,    // Compress rotated log files
+		})
+	} else {
+		// Default to stdout
+		Logger.SetOutput(os.Stdout)
+	}
+
+	// Initialize Sentry
+	if sentryDSN != "" {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn: sentryDSN,
+		})
+		if err != nil {
+			Logger.Fatalf("Failed to initialize Sentry: %v", err)
+		}
+	}
 }
 
-// LogDebug logs a debug message
-func LogDebug(message string, fields map[string]interface{}) {
-	Logger.WithFields(fields).Debug(message)
-}
-
-// LogInfo logs an info message
-func LogInfo(message string, fields map[string]interface{}) {
-	Logger.WithFields(fields).Info(message)
-}
-
-// LogWarn logs a warning message
-func LogWarn(message string, fields map[string]interface{}) {
-	Logger.WithFields(fields).Warn(message)
-}
-
-// LogError logs an error message
+// LogError logs an error message and sends it to Sentry
 func LogError(message string, fields map[string]interface{}) {
 	Logger.WithFields(fields).Error(message)
+
+	// Send error to Sentry
+	if sentry.CurrentHub().Client() != nil {
+		sentry.WithScope(func(scope *sentry.Scope) {
+			for key, value := range fields {
+				scope.SetExtra(key, value)
+			}
+			sentry.CaptureMessage(message)
+		})
+	}
 }
 
-// LogFatal logs a fatal message and exits the program
+// LogFatal logs a fatal message, sends it to Sentry, and exits the program
 func LogFatal(message string, fields map[string]interface{}) {
 	Logger.WithFields(fields).Fatal(message)
-}
 
-// LogPanic logs a panic message and panics
-func LogPanic(message string, fields map[string]interface{}) {
-	Logger.WithFields(fields).Panic(message)
+	// Send fatal error to Sentry
+	if sentry.CurrentHub().Client() != nil {
+		sentry.WithScope(func(scope *sentry.Scope) {
+			for key, value := range fields {
+				scope.SetExtra(key, value)
+			}
+			sentry.CaptureMessage(message)
+		})
+	}
+	os.Exit(1)
 }
