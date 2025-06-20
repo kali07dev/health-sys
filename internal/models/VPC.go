21edb987fffe53418ed40af1ca4407db787513af
Hope <hopakalitera@gmail.com>
2025-06-20 12:17:21 +0200

    fix: remove check constraint from VpcType field in VPC model

--- internal/models/VPC.go
+++ internal/models/VPC.go
@@ -13,7 +13,7 @@ type VPC struct {
 	ReportedDate      time.Time `gorm:"not null"`
 	Department        string    `gorm:"type:varchar(50);not null"`
 	Description       string    `gorm:"type:text;not null"`
-	VpcType           string    `gorm:"type:varchar(50);not null; check:vpc_type IN ('safe', 'unsafe')"`
+	VpcType           string    `gorm:"type:varchar(50);not null;"`
 	ActionTaken       string    `gorm:"type:text;not null"`
 	IncidentRelatesTo string    `gorm:"type:varchar(50);not null"`
 	CreatedBy         uuid.UUID `gorm:"type:uuid;"`
